7df53875736d6c1012955ca4f3092fa37ec7ccfd
Subbu Muthu <you@example.com>
2021-01-24 14:37:09 -0700

    edit search page feature

--- src/main/java/com/qa/pages/Loginpage.java
+++ src/main/java/com/qa/pages/Loginpage.java
@@ -1,5 +1,14 @@
 package com.qa.pages;
 
 public class Loginpage {
+	public void login()
+	{
+		System.out.println("login");
+	}
+	
+	public void loginset()
+	{
+		System.out.println("loginset");
+	}
 
 }
